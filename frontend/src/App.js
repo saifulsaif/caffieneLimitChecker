17ec9b5cc6b0f471fd2eb6de61ec0336292496a9
saifulsaif <saifulsaif5854@gmail.com>
2022-10-10 14:58:30 +0600

    Revert "transfer code"
    This reverts commit d6cf2551cd2b736b5d263c9955ce4dc622b367ea.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,117 +1,15 @@
-import React, { useState, useEffect } from "react";
-import "./component/dashboard/Dashboard.css";
-import ProductCard from "./component/dashboard/ProductCard";
-import axios from "axios";
-import Wishlist from "./component/dashboard/wishlist";
-import HeaderCard from "./component/dashboard/HeaderCard";
-import Profile from "./component/dashboard/Profile";
-const api_url = process.env.REACT_APP_API_URL;
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-const App = () => {
-  const [products, setProducts] = useState([]);
-  const [profile, setProfile] = useState([]);
-  const [wish, setWish] = useState([]);
-
-  const [limit, setLimit] = useState(300);
-
-  useEffect(() => {
-    fetch_data();
-  }, []);
-
-  function fetch_data() {
-    axios.get(api_url + "/products").then((res) => {
-      setProducts(res.data.data);
-    });
-
-    axios.get(api_url + "/user").then((res) => {
-      setProfile(res.data.data);
-      setLimit(res.data.data.c_limit);
-    });
-
-    axios.get(api_url + "/wish_list").then((res) => {
-      setWish(res.data.data);
-    });
-  }
-
-  var total_caff = 0;
-  var process_color = "progress-bar bg-success";
-  var status = "Excelent";
-  wish.map((wish) => {
-    total_caff += wish.qty * wish.caffiene;
-  });
-
-  var process = (100 / limit) * total_caff;
-  var remmining_limit = limit - total_caff;
-  if (process >= 60 && process < 80) {
-    process_color = "progress-bar bg-warning";
-    status = "Over";
-  } else if (process >= 80) {
-    process_color = "progress-bar bg-danger";
-    status = "Risky";
-  }
-
+function App() {
   return (
-    <div>
-      <div className="row">
-        <div className="header">
-          <h1>Caffiene Limit Checker</h1>
-        </div>
-        <div class="progress">
-          <div
-            class={process_color}
-            role="progressbar"
-            style={{ width: process + "%" }}
-            aria-valuenow={process}
-            aria-valuemin="0"
-            aria-valuemax={limit}
-          >
-            Caffiene {process} %
-          </div>
-        </div>
-      </div>
-      <div className="container">
-        <div className="row mt-4">
-          <HeaderCard
-            limit={limit + "gm"}
-            title={"User Caffiene Limit"}
-            icon={"fa fa-send"}
-          />
-          <HeaderCard
-            limit={total_caff + "gm"}
-            title={"You have consume"}
-            icon={"fa fa-file"}
-          />
-          <HeaderCard
-            limit={remmining_limit + "gm"}
-            title={" Remining Caffiene"}
-            icon={"fa fa-user"}
-          />
-          <HeaderCard
-            limit={status}
-            title={" Limit Status"}
-            icon={"fa fa-user"}
-          />
-        </div>
-        <div className="row mt-4">
-          <div className="col-md-3 col-ms-12">
-            <Profile item={profile} />
-          </div>
-          <div className="col-md-9 col-ms-12">
-            <div className="row">
-              <div className="col-md-12">
-                <Wishlist item={wish} total={total_caff} />
-              </div>
-            </div>
-            <div className="row">
-              {products.map((product) => (
-                <ProductCard item={product} limit={remmining_limit} />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
-};
+}
 
 export default App;
